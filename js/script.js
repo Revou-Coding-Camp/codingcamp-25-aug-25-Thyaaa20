730d3e58a255d9451fe0da313d6c966fc21d7288
Thyaaa20 <raffiradithya20@gmail.com>
2025-08-29 23:40:05 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,23 +1,22 @@
-// Greeting
 function greet() {
   let name = prompt("Masukkan Nama Anda : ");
   document.getElementById('username').innerText = name || "Guest";
 }
 greet();
 
-// Handle form submit
-document.getElementById("messageForm").addEventListener("submit", function(e) {
+
+document.getElementById("contactForm").addEventListener("submit", function(e) {
   e.preventDefault();
 
   const name = document.getElementById("name").value;
-  const date = document.getElementById("date").value;
-  const gender = document.getElementById("gender").value;
+  const date = document.getElementById("dob").value;
+  const gender = document.querySelector("input[name='gender']:checked").value;
   const message = document.getElementById("message").value;
   const time = new Date().toLocaleTimeString();
 
   const output = `
     <p><strong>Name:</strong> ${name}</p>
-    <p><strong>Date:</strong> ${date}</p>
+    <p><strong>Date of Birth:</strong> ${date}</p>
     <p><strong>Gender:</strong> ${gender}</p>
     <p><strong>Message:</strong> ${message}</p>
     <p><em>Submitted at: ${time}</em></p>
